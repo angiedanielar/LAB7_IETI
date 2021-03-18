699095ce5583ab0c5138d8ddf58d9958529c6ecb
Daniela <angied.ruiz@gmail.com>
2021-03-17 19:26:35 -0500

    parte dos

--- list-tasks/index.js
+++ list-tasks/index.js
@@ -1,20 +1,12 @@
-var cont = 0;
-
 var app = require("../App");
 
 module.exports = async function (context, req) {
-	cont =++ cont;
-
-    const task = req.body;
-	task.id = cont;
-	
-	app.push(task);
 	
     // Implement the response
-    const responseMessage = { response: task};
+    const responseMessage = { response: app.get()};
 
     context.res = {
-        status: 201,
+        status: 200,
         body: responseMessage
     };
 }
